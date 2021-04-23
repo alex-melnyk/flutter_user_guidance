f3b05bf4dcb1c8ee917e6be4292149341a0d1a7c
Alex Melnyk <ask.melnyk@gmail.com>
2021-04-23 20:04:57 +0300

    Code improvements

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_user_guidance/widgets/user_guidance.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -14,8 +15,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-  final _textTyping = 'Tap on (+) button to increase the number';
-
+  final _userGuidanceController = UserGuidanceController();
   AnimationController _animationController;
   Animation _animation;
   int _counter = 0;
@@ -44,6 +44,13 @@ class _HomePageState extends State<HomePage>
         Scaffold(
           appBar: AppBar(
             title: Text('Flutter User Guidance Example'),
+            centerTitle: false,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.slideshow),
+                onPressed: () => _userGuidanceController.show(),
+              ),
+            ],
           ),
           body: Center(
             child: Column(
@@ -65,72 +72,8 @@ class _HomePageState extends State<HomePage>
             child: Icon(Icons.add),
           ),
         ),
-        Positioned.fill(
-          child: ColorFiltered(
-            colorFilter: ColorFilter.mode(
-              Colors.black87,
-              BlendMode.srcOut,
-            ),
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    backgroundBlendMode: BlendMode.dstOut,
-                  ),
-                ),
-                Positioned(
-                  bottom: 22,
-                  right: 4,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      top: 80,
-                    ),
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Material(
-          color: Colors.transparent,
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: EdgeInsets.only(
-                bottom: 160,
-              ),
-              padding: EdgeInsets.symmetric(
-                horizontal: 24.0,
-              ),
-              width: double.maxFinite,
-              child: AnimatedBuilder(
-                animation: _animation,
-                builder: (context, child) {
-                  final textLength = StepTween(
-                    begin: 0,
-                    end: _textTyping.length,
-                  ).evaluate(_animationController);
-
-                  return Text(
-                    _textTyping.substring(0, textLength),
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                      color: Colors.white,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
+        UserGuidance(
+          controller: _userGuidanceController,
         ),
       ],
     );
